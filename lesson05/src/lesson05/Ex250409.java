311d8302aad4f78e1a9244b34c4ef79b3115fbd1
jin-lgtm <cadcg0328@gmail.com>
2025-04-09 23:25:59 +0900

    최초 커밋

--- lesson05/src/lesson05/Ex250409.java
+++ lesson05/src/lesson05/Ex250409.java
@@ -10,64 +10,71 @@ public class Ex250409 {
 		// 그 아이디가 admin, 비밀번호가 1234 일때 로그인 성공
 		// 아이디가 admin 비밀번호가 틀렸을때 로그인 실패 > 잘못된 번호
 		// 아이디가 admin이 아니면 없는 계정 메세지 출력
-		System.out.println("아이디를 입력하세요 : ");
-//		Scanner scan = new Scanner(System.in);
-		System.out.println("비밀번호를 입력하세요 : ");
-		Scanner scan = new Scanner(System.in);
-		
-		String idin = scan.nextLine();
-		String pwin = scan.nextLine();
-		String id = "admin";
-		String pw = "1234";
+		Scanner scanner = new Scanner(System.in);
 
-		if(id == idin && pw == pwin) {
+		System.out.println("아이디를 입력하세요 : ");
+		String userId = scanner.nextLine();
+		
+		System.out.println("비밀번호를 입력하세요 : ");
+		String userPw = scanner.nextLine();
+
+		if(admin == userId && 1234 == userPw) {
 			System.out.println("로그인 성공");
 		}
-		else if(id == idin && pw != pwin) {
+		else if(admin == userId && 1234 != userPw) {
 			System.out.println("로그인 실패 : 잘못된 번호");
 		}
-		if(id != idin) {
+		if(admin != userId) {
 			System.out.println("로그인 실패 : 없는 계정");
 		}
-		
-		
-		
 		
 		
 		// 2. 숫자 하나를 입력받아서
 		// 3의 배수 그리고 짝수 여부를 동시판별
 		// 3의 배수 , 짝수, 3의 배수이면서 짝수, 둘다 아닌경우
-				
+		Scanner scanner = new Scanner(System.in);
 		
-			
+		System.out.println("숫자를 입력하세요 : ");
+	 	int num = scanner.nextInt();
+		if(num % 3 == 0 && num % 2 == 0) {
+			System.out.println(" 3의 배수 이면서 짝수 ");
+		}
+		else if (num % 3 == 0) {
+			System.out.println(" 3의 배수 ");
+		}
+		else if (num % 2 == 0) {
+			System.out.println(" 짝수 ");
+		}
+		else {
+			System.out.println(" 둘 다 아님 ");
+		}
 		
 		
 		// 3. 월(month)을 입력받아서 계절을 출력
 		// 예시  3~5월은 봄
-		System.out.println("월을 입력하세요 : ");
-		Scanner scan1 = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		
-		int m = scan1.nextInt();
-		String month = "";
-		switch (m) {
-			case 12: case 1: case 2:
-				month = "겨울";
-				break;
-			case 3: case 4: case 5:
-				month = "봄";
-				break;
-			case 6: case 7: case 8:
-				month = "여름";
-				break;
-			case 9: case 10: case 11:
-				month = "가을";
-				break;
-			default :
-				month = "잘못입력";
+		System.out.println(" 월을 입력  : ");
+		int month = scanner.nextInt();
+		
+		String season;
+		
+		if (month >= 3 && month <= 5) {
+			season = "봄";
 		}
-		
-		
-		
+		else if (month >= 6 && month <= 8) {
+			seson = "여름";
+		}
+		else if (month >= 9 && month <= 11) {
+			seson = "가을";
+		}
+		else if (month == 12 || month == 1 || month ==2) {
+			seson = "겨울"
+		}
+		else {
+			seson = " 잘못 입력 "
+		}
+		System.out.println(" 계절  : " + seson);		
 		
 	}
 }
